3ac6543ab6024fea599d3b936dd1b9e032c5a8e3
UltimatePerson <112718587+UltimatePerson@users.noreply.github.com>
2022-09-05 12:20:01 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var marks = [35,42,38,19,49,20,27,43,21,46];
+console.log (marks);
+var sum = marks[0]+marks[1]+marks[2]+marks[3]+marks[4]+marks[5]+marks[6]+marks[7]+marks[8]+marks[9]
+var average = sum / marks.length
+console.log (sum);
+console.log (average);
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
